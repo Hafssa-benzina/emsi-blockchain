a1376bd8ce06db26ebcb17cb4c2d679a97ec0c22
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-17 17:42:16 +0000

    Update blockchain_create.c

--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -4,21 +4,13 @@
 #define GENESIS_DATA_LEN 16
 #define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
 /**Create a blockchain
-*The Blockchain must contain one block upon creation. This block is called the Genesis Block. Its content is static, and pre-defined as follows:
-*index -> 0
-*difficulty -> 0
-*timestamp -> 1537578000
-*nonce -> 0
-*prev_hash -> 0 (times 32)
-*data -> "Holberton School" (16 bytes long)
-*hash -> c52c26c8b5461639635d8edf2a97d48d0c8e0009c817f2b1d3d7ff2f04515803
-*/
+*The Blockchain must contain one block upon creation. This block is called the Genesis Block. Its content is static*/
 blockchain_t *blockchain_create(void){
 /*creating a list of blocks*/
 	blockchain_t *list=calloc(1,sizeof(*list));
-	block_t *Genesis=calloc(1,sizeof(*Genesis));
 	if(list==NULL)
 	  return (NULL);
+	block_t *Genesis=calloc(1,sizeof(*Genesis));
 	 if(Genesis==NULL){
 	  	free(Genesis);
 	  	return (NULL);
@@ -43,5 +35,5 @@ blockchain_t *blockchain_create(void){
 		free(list);
 		return (NULL);
 	}
-	return list;
+	return (list);
 }
