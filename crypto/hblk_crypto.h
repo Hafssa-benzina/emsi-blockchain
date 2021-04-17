6dbeec1a5072b159f9c343008c8d4c2a520ebab5
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-17 11:57:18 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,6 +1,5 @@
 #ifndef _HBLK_CRYPTO_H_
 #define _HBLK_CRYPTO_H_
-
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/obj_mac.h>
@@ -12,17 +11,13 @@
 #include <openssl/ecdsa.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #define PUB_FILE "key_pub.pem"
 #define PRV_FILE "key.pem"
-
 #define EC_CURVE NID_secp256k1
-
 /* EC_KEY public key octet string length (using 256-bit curve) */
 #define EC_PUB_LEN 65
 /* Maximum signature octet string length (using 256-bit curve) */
 #define SIG_MAX_LEN 72
-
 /**
  * struct sig_s - EC Signature structure
  *
@@ -39,9 +34,7 @@ typedef struct sig_s
 	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
 } sig_t;
-
-uint8_t *sha256(int8_t const *s, size_t len,
-		uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
@@ -51,5 +44,4 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 		 size_t msglen, sig_t *sig);
 uint ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	       sig_t const *sig);
-
 #endif 
