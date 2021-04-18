27ad109800eb3e703c8e87e7c28059b144da8bd7
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-18 18:31:01 +0000

    Update block_destroy.c

--- blockchain/v0.1/block_destroy.c
+++ blockchain/v0.1/block_destroy.c
@@ -1,12 +1,9 @@
 #include "blockchain.h"
 
 /**
- * A function that deletes an existing Blockchain
- * along with all the Blocks it contains
- * blockchain: point to the Blockchain structure to delete
+ * A function that deletes an existing block
  */
 void block_destroy(block_t *block);
 {
-	llist_destroy(block_t->chain, 1, (void (*)(llist_node_t))block_destroy);
-	free(block_t);
+	free(block);
 }
