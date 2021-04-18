e04ce5b0ad947174aebe8da610c2118c78fdb3fa
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-18 22:06:47 +0000

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -2,19 +2,13 @@
 
 /**
  * block_create - create a Block structure and initializes it
- * @prev: a pointer to the previous Block in the Blockchain
- * @data: points to a memory area to duplicate in the Block’s data
- * @data_len: the number of bytes to duplicate in data
  */
-block_t *block_create(block_t const *prev, int8_t const *data,
-		      uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len)
 {
 	block_t *blk;
-
 	blk = calloc(1, sizeof(*blk));
 	if (blk == NULL)
 		return (NULL);
-
 	blk->info.index = prev->info.index + 1;
 	blk->info.difficulty = 0;
 	blk->info.timestamp = time(NULL);
