4a4717fbc4987a2d4b8986c81184c037f7d13d8b
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-23 23:04:34 +0000

    Update hblk_crypto.h

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -34,8 +34,8 @@ typedef struct sig_s
 	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
 } sig_t;
-uint8_t *sha256(int8_t const *s, size_t len,
-		 uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len, 
+		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
