02fad56b028c08e91c6b107b0ba01d18b323c73c
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-23 23:16:23 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,7 +1,8 @@
 #include "hblk_crypto.h"
+
 /**
- * sha256 - a function that computes the hash of a sequence of bytes
- * Return: a pointer to digest or NULL if digest happens to be NULL
+ * A function that computes the hash of a sequence of bytes
+ * Return a pointer to digest or NULL if digest happens to be NULL
  */
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH])
