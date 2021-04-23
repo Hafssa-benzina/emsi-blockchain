9a71353f47927f7e50cf3c93dee08d43177c92cc
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-23 23:07:55 +0000

    Update sha256.c

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -3,7 +3,7 @@
  * sha256 - a function that computes the hash of a sequence of bytes
  * Return: a pointer to digest or NULL if digest happens to be NULL
  */
-uint8_t *sha256(int8_t const *s, size_t len, 
+uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (digest == NULL)
