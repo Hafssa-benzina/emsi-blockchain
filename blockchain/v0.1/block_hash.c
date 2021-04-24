549bef37a95dac57c152e9a03eb4ce74b2f8c6c0
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-24 23:20:08 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -7,7 +7,8 @@
  *
  * Return: a pointer to hash_buf
  */
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	sha256((int8_t const *)&(block->info), 4 + 4 + 8 + 8 + 32 + block->data.len, hash_buf);
 	return (hash_buf);
