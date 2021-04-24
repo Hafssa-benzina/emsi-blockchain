5bf77868cb7e7f63d4778052dffbbda8dc60c8a5
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-24 23:09:50 +0000

    Update block_create.c

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -1,4 +1,5 @@
 #include "blockchain.h"
+
 /**
  * block_create - create a Block structure and initializes it
  * @prev: a pointer to the previous Block in the Blockchain
@@ -7,20 +8,22 @@
  *
  * Return: a pointer to an EC_KEY structure or NULL upon failure
  */
-block_t *block_create(block_t const *prev,
-		      int8_t const *data, uint32_t data_len)
+block_t *block_create(block_t const *prev, int8_t const *data,
+		      uint32_t data_len)
 {
-	block_t *blk;
-	blk = calloc(1, sizeof(*blk));
+	block_t *bll;
+
+	b = calloc(1, sizeof(*blk));
 	if (blk == NULL)
 		return (NULL);
+
 	blk->info.index = prev->info.index + 1;
 	blk->info.difficulty = 0;
 	blk->info.timestamp = time(NULL);
 	blk->info.nonce = 0;
 	memcpy(blk->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
 	memcpy(blk->data.buffer, data, data_len);
-	blk->data.len = data_len;
+	b->data.len = data_len;
 	memset(blk->hash, 0, SHA256_DIGEST_LENGTH);
 	return (blk);
 }
