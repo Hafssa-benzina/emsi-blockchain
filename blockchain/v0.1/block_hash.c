de889415beaba362b0c05e5039c39c88256e9ebf
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-24 23:18:40 +0000

    Update block_hash.c

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -2,6 +2,9 @@
 
 /**
  * block_hash - a function that computes the hash of a Block
+ * @block: point to the Block to be hashed
+ * @hash_buf:The resulting hash must be stored in hash_buf
+ *
  * Return: a pointer to hash_buf
  */
 uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
