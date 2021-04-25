a96b3dd51627b9dc2cfab50f9789496d1b22ed68
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-25 21:41:14 +0000

    Update ec_save.c

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,9 +1,10 @@
 #include "hblk_crypto.h"
 
 /**
- *A function that saves an existing EC key pair on the disk
+ * ec_save - a function that saves an existing EC key pair on the disk
  * @key: points to the EC key pair to be saved on disk
  * @folder: the path to the folder in which to save the keys
+ *
  * Return: 1 or 0 upon success or failure
  */
 int ec_save(EC_KEY *key, char const *folder)
