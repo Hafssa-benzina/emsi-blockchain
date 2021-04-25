4d862fa0ec3e1cc0c095e5e4048ee89514f65b51
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-25 21:39:55 +0000

    Update ec_from_pub.c

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -1,12 +1,11 @@
 #include "hblk_crypto.h"
 
 /**
- * A function that creates an EC_KEY structure given a public key
+ * ec_from_pub - a function that creates an EC_KEY structure given a public key
  * @pub: the public key to be converted
  *
  * Return: a pointer to the created EC_KEY structure, or NULL upon failure
  */
-
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
 	EC_POINT *pub_key;
