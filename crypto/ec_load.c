0610568f2089ba7124fd51a67a31c1414d545280
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-04-30 03:55:13 +0000

    Update ec_load.c

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,33 +1,44 @@
 #include "hblk_crypto.h"
+
 /**
- * ec_load - function load public key and privite key from file
- * @folder: the path where files are saved
- * Return: the EC_KEY contaning the public key and privite key
-*/
+ * ec_load - a function that loads an EC key pair from the disk
+ * @folder: the path to the folder from which to load the keys
+ *
+ * Return: a pointer to the created EC key pair, or NULL upon failure
+ */
 EC_KEY *ec_load(char const *folder)
 {
-	EC_KEY *mykey = NULL;
-	char myfile[512] = {0};
-	FILE *pfile;
-	struct stat st;
+	FILE *fp;
+	char path[128] = {0};
+	EC_KEY *key = NULL;
 
 	if (!folder)
-		return (NULL);
-	if (stat(folder, &st) == -1)
-		return (NULL);
-	sprintf(myfile, "./%s/%s", folder, PUB_FILENAME);
-	pfile = fopen(myfile, "r");
-	if (!pfile)
-		return (NULL);
-	if (!PEM_read_EC_PUBKEY(pfile, &mykey, NULL, NULL))
-		return (NULL);
-	fclose(pfile);
-	sprintf(myfile, "./%s/%s", folder, PRI_FILENAME);
-	pfile = fopen(myfile, "r");
-	if (!pfile)
-		return (NULL);
-	if (!PEM_read_ECPrivateKey(pfile, &mykey, NULL, NULL))
-		return (NULL);
-	fclose(pfile);
-	return (mykey);
+		return (0);
+
+	sprintf(path, "%s/" PUB_FILE, folder);
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		EC_KEY_free(key);
+		return (0);
+	}
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+
+	sprintf(path, "%s/" PRV_FILE, folder);
+	fp = fopen(path, "r");
+	if (!fp)
+		return (0);
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	return (key);
 }
