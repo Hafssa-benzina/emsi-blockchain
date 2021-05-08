00d5c3d51180caeb6c791024fb042d07fc55ac4a
Hafssa-benzina <80366662+Hafssa-benzina@users.noreply.github.com>
2021-05-08 10:07:48 +0000

    Update tx_in_sign.c

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -6,7 +6,6 @@
  * @tx_id: id of transaction containing @in
  * @sender: private key of tx receiver
  * @all_unspent: list of unspent tx outputs to date
- *
  * Return: pointer to resulting sig struct or null
  */
 sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
@@ -14,20 +13,20 @@ sig_t *tx_in_sign(tx_in_t *in, uint8_t const tx_id[SHA256_DIGEST_LENGTH],
 {
 	uint8_t pub[EC_PUB_LEN];
 	ssize_t i;
-	unspent_tx_out_t *unspent_tx_out;
+	unspent_tx_out_t *utx;
 
 	for (i = 0; i < llist_size(all_unspent); i++)
 	{
-		unspent_tx_out = llist_get_node_at(all_unspent, i);
-		if (!memcmp(in->tx_out_hash, unspent_tx_out->out.hash,
-			SHA256_DIGEST_LENGTH))
+		utx = llist_get_node_at(all_unspent, i);
+		if (!memcmp(in->tx_out_hash, utx->out.hash, SHA256_DIGEST_LENGTH))
 			break;
-		unspent_tx_out = NULL;
+		utx = NULL;
 	}
-	if (!unspent_tx_out || !ec_to_pub(sender, pub)
-		|| memcmp(pub, unspent_tx_out->out.pub, EC_PUB_LEN))
+
+	if (!utx || !ec_to_pub(sender, pub) || memcmp(pub, utx->out.pub, EC_PUB_LEN))
 		return (NULL);
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
+
 	return (&in->sig);
 }
